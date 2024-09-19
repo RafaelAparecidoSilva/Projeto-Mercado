e80db415b8d525982689b3ef30c20d420ff83727
Rafael Silva <raparecido@img.com.br>
2024-09-18 23:51:57 -0300

    Implantação das funções do menu, cadastro de produto, lista de produto e fechamento de pedido

--- mercado.py
+++ mercado.py
@@ -7,26 +7,94 @@ from utils.helper import formata_float_str_moeda
 produtos: List[Produto] = []
 carrinho: List[Dict[Produto, int]] = []
 
+
 def main() -> None:
     menu()
 
-def menu() -> None:
-    pass
 
-def cadastro_produto() -> None:
-    pass
+def menu() -> None:
+    print("\n")
+    print("=" * 30)
+    print(" Projeto Mercado ".center(30, "="))
+    print("=" * 30 + "\n")
+
+    print("Selecione uma das opções abaixo:")
+    print("1 - Cadastrar produto")
+    print("2 - Listar produto")
+    print("3 - Comprar produto")
+    print("4 - Visualizar carrinho")
+    print("5 - Fechar pedido")
+    print("6 - Sair do sistema")
+
+    opcao: int = int(input("\nInforme sua escolha: "))
+
+    match opcao:
+        case 1:
+            cadastrar_produto()
+        case 2:
+            listar_produtos()
+        case 3:
+            comprar_produto()
+        case 4:
+            visualizar_carrinho()
+        case 5:
+            fechar_pedido()
+        case 6:
+            print("Volte sempre!")
+            sleep(2)
+            exit(0)
+        case _:
+            print('Opção inválida')
+            sleep(1)
+            menu()
+
+
+def cadastrar_produto() -> None:
+    print("Cadastro de Produto")
+    print("=" * len("Cadastro de Produto"))
+
+    nome: str = input("Informe o nome do produto: ")
+    preco: float = float(input("Informe o preço do produto: "))
+
+    produto: Produto = Produto(nome, preco)
+    produtos.append(produto)
+
+    print(f"O produto {produto.nome} foi cadastrado com sucesso!")
+    sleep(2)
+    menu()
+
 
 def listar_produtos() -> None:
-    pass
+    if len(produtos) > 0:
+        print("Listagem de produtos")
+        print("=" * len("Listagem de produtos"))
+
+        for produto in produtos:
+            print(produto)
+            print("-" * 40)
+            sleep(1)
+    else:
+        print("Ainda não existe produtos cadastrados.")
+    sleep(2)
+    menu()
+
 
 def comprar_produto() -> None:
     pass
 
+
 def visualizar_carrinho() -> None:
     pass
 
+
 def fechar_pedido() -> None:
-    pass
+    if(carrinho) > 0:
+        pass
+    else:
+        print("Ainda não existem produtos no carrinho.")
+    sleep(2)
+    menu()
+
 
 def pega_produto_por_codigo() -> Produto:
     pass
